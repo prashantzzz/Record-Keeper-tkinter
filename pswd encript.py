5474953ef053e451244de54230377776d1692825
Prashant <60565847+prashantzzz@users.noreply.github.com>
2023-08-06 17:34:27 +0530

    Update pswd encript.py

--- pswd encript.py
+++ pswd encript.py
@@ -1,4 +1,5 @@
-pswd1='1512003pk*'
+pswd1='1512003pk*'  #contains pre filled password
+#below are the replacements for each letter in such a manner so that same letters may form the same encrypted code, making it even harder to crack.
 dict1={'1':'qm','2':'wn','3':'eb','4':'rv','5':'tc','6':'yx','7':'uz','8':'il','9':'ok','0':'pj',
        'q':'a1','w':'b2','e':'c3','r':'d4','t':'e5','y':'e6','u':'f7','i':'g8','o':'h9','p':'j1',
        'a':'k2','s':'l3','d':'m4','f':'n5','g':'o6','h':'p7','j':'q8','k':'r9','l':'s1','z':'t2',
